b91ea6987a475daefb8e1e0c9cb4bdacd8f70eb9
Collin Kennedy <collinjkennedy@gmail.com>
2021-03-03 10:35:58 -0800

    updated cron job

--- cron.R
+++ cron.R
@@ -1,24 +1,28 @@
 #
 library(DBI)
 library(tidyverse)
+library(reader)
 
 host <- "34.123.54.80"  # replace it with your server ip
+NASDAQ <- read.delim("~/Google Drive/Programming Folder (4Transfer to Collin's Files V2)/R/STA141B/data_technologies_project/NASDAQ.txt")
 
 con <- dbConnect(
   RPostgres::Postgres(),
-  dbname = "demo",
+  dbname = "postgres",
   user = "postgres", password = Sys.getenv("DATABASEPW"), host = host)
 
-?dbWriteTable
-if (!("table1" %in% dbListTables(con))) {
+
+if (!("tickerTable" %in% dbListTables(con))) {
   # create table if it doesn't exist
   dbWriteTable(
     con,
-    "table1",
-    data.frame(x = rnorm(10), y = rnorm(10))#fix this
+    "tickerTable",
+    NASDAQ#fix this
   )
 }
 
+con %>% dbListTables()
+con %>% tbl("table1")
 
 # randomly generate some data
 data <- data.frame(x = rnorm(10), y = rnorm(10))
